725de2bf03c4aa0b9e365e934d090a3641f768b2
sarabastian <seb118@georgetown.edu>
2020-11-20 14:36:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+
+    
+function submitData(name, email) {
+
+   return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+    body: JSON.stringify({
+        name,
+        email
+    })
+  })
+   .then(function(response) {
+       return response.json();
+   })
+   .then(function(object) {
+       
+       document.body.innerHTML = object["id"]
+   })
+   .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    document.body.innerHTML = error.message;
+  });
+        
+}
+
+
